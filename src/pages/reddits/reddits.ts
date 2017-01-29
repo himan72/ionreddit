a6770d347c601ce33e8fa6de0bad50676b3c6059
Mohammed <admin@joomlar.net>
2017-01-29 22:39:53 +0000

    ok

--- src/pages/reddits/reddits.ts
+++ src/pages/reddits/reddits.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { RedditService } from '../../providers/reddit-service';
+import { DetailsPage } from '../details-page/details-page';
 
 /*
   Generated class for the Reddits page.
@@ -12,11 +14,26 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'reddits.html'
 })
 export class RedditsPage {
+  items: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private redditService: RedditService ) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RedditsPage');
+  }
+  ngOnInit(){
+    this.getPosts('sports', 5);
+
+  }
+  getPosts(category, limit) {
+    this.redditService.getPosts(category, limit).subscribe(response => {
+      this.items = response.data.children;
+    });
+  }
+
+  viewItem(item){
+    this.navCtrl.push(DetailsPage, {
+      item:item,
+    })
+
   }
 
 }
